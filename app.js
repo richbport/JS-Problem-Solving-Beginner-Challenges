3d29eed92e4cf338ff8b3686a71d87856b785c0b
Rich <richbport@yahoo.com>
2025-04-22 14:16:09 -0400

    else if

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ function calcTriangleArea(base, height) {
 
 console.log(calcTriangleArea(20, 20));
 
-let bool = !true
-console.log(bool)
+let bool = !true;
+console.log(bool);
+
+let subscribed = false;
+let loggedIn = true;
+
+if (subscribed === true) {
+  console.log("show the video");
+} else if (loggedIn === true) {
+  console.log("tell the user to upgrade their subscription");
+}
