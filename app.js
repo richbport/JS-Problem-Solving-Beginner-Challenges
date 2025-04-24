a55ecc673f3d69b68492f1f70b4adbbfc5c9ce45
Rich <richbport@yahoo.com>
2025-04-24 19:28:25 -0400

    for loop

--- app.js
+++ app.js
@@ -35,8 +35,12 @@ console.log(str);
 //   count = count + 1;
 // }
 
-let count = 1;
-while (count <= 100) {
-  console.log(count);
-  count = count + 1;
-}
+// let count = 1;
+// while (count <= 100) {
+//   console.log(count);
+//   count = count + 1;
+// }
+
+for (let i = 0; i < 3; i++) {
+    console.log(i+1);
+    }
