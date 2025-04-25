e94b02ac19412a0f640b8ceaa5af670fbe34ee2e
Rich <richbport@yahoo.com>
2025-04-25 09:33:53 -0400

    dynamic for loop

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-function calcTriangleArea(base, height) {
-  return (1 / 2) * base * height;
-}
+// function calcTriangleArea(base, height) {
+//   return (1 / 2) * base * height;
+// }
 
-console.log(calcTriangleArea(20, 20));
+// console.log(calcTriangleArea(20, 20));
 
-let bool = !true;
-console.log(bool);
+// let bool = !true;
+// console.log(bool);
 
-let subscribed = false;
-let loggedIn = true;
+// let subscribed = false;
+// let loggedIn = true;
 
-if (subscribed === true) {
-  console.log("show the video");
-} else if (loggedIn === true) {
-  console.log("tell the user to upgrade their subscription");
-} else {
-  console.log("here's your chance to log into the account");
-}
+// if (subscribed === true) {
+//   console.log("show the video");
+// } else if (loggedIn === true) {
+//   console.log("tell the user to upgrade their subscription");
+// } else {
+//   console.log("here's your chance to log into the account");
+// }
 
-let hot = "";
+// let hot = "";
 
-hot ? console.log("weather is hot") : console.log("weather is cold");
+// hot ? console.log("weather is hot") : console.log("weather is cold");
 
-let cash = 50;
-let price = 40;
-let isStoreOpen = true;
+// let cash = 50;
+// let price = 40;
+// let isStoreOpen = true;
 
-let str = cash >= price && isStoreOpen ? "give receipt" : "do not give receipt";
-console.log(str);
+// let str = cash >= price && isStoreOpen ? "give receipt" : "do not give receipt";
+// console.log(str);
 
 // let count = 1;
 // while (count <= 3) {
@@ -47,11 +47,16 @@ console.log(str);
 //   }
 // }
 
-for (let i = 1; i <=20; ++i) {
-    if (i % 3 === 0) {
-        console.log(`${i} -> Frontend`);
-    }
-    else {
-        console.log(`${i} -> ${i}`)
-    }
+// for (let i = 1; i <= 20; ++i) {
+//   if (i % 3 === 0) {
+//     console.log(`${i} -> Frontend`);
+//   } else {
+//     console.log(`${i} -> ${i}`);
+//   }
+// }
+
+const str = "Frontend Simplified";
+
+for (let i = 0; i < str.length; ++i) {
+  console.log(str[i]);
 }
