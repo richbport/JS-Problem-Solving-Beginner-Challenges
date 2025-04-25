4e20a0944618b8b1d9220564d5a2061a5c9e2976
Rich <richbport@yahoo.com>
2025-04-24 20:54:46 -0400

    if and else statement

--- app.js
+++ app.js
@@ -41,8 +41,17 @@ console.log(str);
 //   count = count + 1;
 // }
 
-for (let i = 0; i <= 20; i++) {
-  if (i % 3) {
-    console.log(i);
-  }
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+for (let i = 1; i <=20; ++i) {
+    if (i % 3 === 0) {
+        console.log(`${i} -> Frontend`);
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
 }
