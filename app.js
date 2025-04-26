31280cb48bb8e63cca5aea04b4019035941de453
Rich <richbport@yahoo.com>
2025-04-26 15:02:46 -0400

    function exercise

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ function sumOfTwoNumbers(num1, num2) {
     return num1 / num2;
 }
 
-console.log(sumOfTwoNumbers(10, 30));
+console.log(sumOfTwoNumbers(10, 20));
+
+function convertCelsiusToFarenheit(celsius) {
+    let farenheit = celsius * 1.8 + 32;
+    return farenheit;
+}
+
+console.log(convertCelsiusToFarenheit(0))
+
