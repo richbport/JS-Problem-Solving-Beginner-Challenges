5aca2812641f2f931ba2e441c35f0b05ac02955d
Rich <richbport@yahoo.com>
2025-04-26 18:41:32 -0400

    arrow function

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-function sumOfTwoNumbers(num1, num2) {
-  return num1 / num2;
-}
+// function sumOfTwoNumbers(num1, num2) {
+//   return num1 / num2;
+// }
 
-console.log(sumOfTwoNumbers(10, 20));
-
-function convertCelsiusToFahrenheit(celsius) {
+const convertCelsiusToFahrenheit = (celsius) => {
   return celsius * 1.8 + 32;
-}
+};
 
 console.log(convertCelsiusToFahrenheit(0));
