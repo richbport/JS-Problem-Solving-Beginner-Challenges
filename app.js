86e30b6f3363ef6ea14a162e9d9eb9908f8b5a53
Rich <richbport@yahoo.com>
2025-04-29 11:05:02 -0400

    push method

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let grades = ["A+", "A", "FAIL"];
 
-let goodGrades = grades.filter((element) => element !== "FAIL");
+let goodGrades = [];
+
+for (let i = 0; i < grades.length; ++i) {
+  if (grades[i] !== "FAIL") {
+    goodGrades.push(grades[i]);
+  }
+}
 
 console.log(goodGrades);
