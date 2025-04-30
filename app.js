0be26ec0b371a50c739ced58acb26099c63b6a4f
Rich <richbport@yahoo.com>
2025-04-30 13:14:38 -0400

    objects in an array

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-let user = {
-  username: "Rich",
-  email: "rich@frontendsimplified.com",
-  subscriptionStatus: "VIP",
-  discordId: "Rich Port#0001",
-  lessonsCompleted: [0, 1],
-};
+let users = [
+  {
+    username: "Rich",
+    email: "rich@frontendsimplified.com",
+    subscriptionStatus: "VIP",
+    discordId: "Rich Port#0001",
+    lessonsCompleted: [0, 1],
+  },
+  {
+    username: "Mitri",
+    email: "mitri@frontendsimplified.com",
+    subscriptionStatus: "VIP",
+    discordId: "Mitri#0001",
+    lessonsCompleted: [0, 1],
+  },
+];
 
-console.log(user.lessonsCompleted);
+console.log(users[0]);
