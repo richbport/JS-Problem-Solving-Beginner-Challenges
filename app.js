ada3000847e20c28516bae2b203274458edc3f73
Rich <richbport@yahoo.com>
2025-05-01 11:33:44 -0400

    array exercise

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let users = [
     email: "mitri@frontendsimplified.com",
     subscriptionStatus: "VIP",
     discordId: "Mitri#0001",
-    lessonsCompleted: [0, 1],
+    lessonsCompleted: [0, 1, 2, 3],
   },
 ];
 
-console.log(users[0]);
+console.log(users[0].lessonsCompleted.map(elem => elem * 2));
