009b8ac9bbddbdfb33f1f104ca83eb58a1ea2b69
Rich <richbport@yahoo.com>
2025-05-02 12:43:59 -0400

    function register

--- app.js
+++ app.js
@@ -15,22 +15,24 @@ let users = [
     discordId: "Mitri#0001",
     lessonsCompleted: [0, 1, 2, 3],
   },
+  {
+    username: "Zen",
+    email: "zen@frontendsimplified.com",
+    password: "zen123",
+    subscriptionStatus: "VIP",
+    discordId: "Zen#0001",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
 ];
 
-function login(email, password) {
-  return
-  for (let i = 0; i < users.length; ++i) {
-    if (users[i].email === email) {
-      console.log(users[i]);
-      if (users[i].password === password) {
-        console.log("log the user in - details are correct");
-      } else {
-        console.log("password is incorrect - try again");
-      }
-      return;
-    }
-  }
-  console.log("could not find an email that matches");
+function register(
+  username,
+  email,
+  password,
+  subscriptionStatus,
+  discordId,
+  lessonsCompleted
+) {
+  console.log(email, password);
 }
-
-login("rich@frontendsimplified.com", "test23");
+register("zen@frontendsimplified.com", "zen123");
