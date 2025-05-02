226e84085e126f8bd65e2fbdf5e85420d75b570b
Rich <richbport@yahoo.com>
2025-05-02 12:02:02 -0400

    return function

--- app.js
+++ app.js
@@ -18,17 +18,19 @@ let users = [
 ];
 
 function login(email, password) {
+  return
   for (let i = 0; i < users.length; ++i) {
     if (users[i].email === email) {
       console.log(users[i]);
       if (users[i].password === password) {
         console.log("log the user in - details are correct");
+      } else {
+        console.log("password is incorrect - try again");
       }
-      else {
-        console.log('password is incorrect - try again')
-      }
+      return;
     }
   }
+  console.log("could not find an email that matches");
 }
 
-login("rich@frontendsimplified.com", "test123");
+login("rich@frontendsimplified.com", "test23");
