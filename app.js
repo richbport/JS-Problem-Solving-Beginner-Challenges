24e3e7c16123f638b3d12f4160cc556f6884ff0a
Rich <richbport@yahoo.com>
2025-05-07 14:07:32 -0400

    new Promise

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const emailRef = document.querySelector(".email");
-
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.username;
-//   });
-
-async function main() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-  const data = await response.json();
-  console.log(data);
-  emailRef.innerHTML = data.email;
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    resolve("VIP")
+  })
 }
 
-main();
+console.log(getSubscriptionStatus())
