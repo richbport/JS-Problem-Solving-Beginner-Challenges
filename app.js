2f6e59b1602d32dfb0a075c188468cda0e99043a
Rich <richbport@yahoo.com>
2025-05-08 19:34:06 -0400

    functions getVideo and getSubscriptionStatus

--- app.js
+++ app.js
@@ -8,11 +8,22 @@ function getSubscriptionStatus() {
   });
 }
 
-// getSubscriptionStatus().then((response) => console.log(response));
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
 
 async function main() {
   const status = await getSubscriptionStatus();
   statusRef.innerHTML = status;
+  console.log(await getVideo(status));
 }
 
 main();
