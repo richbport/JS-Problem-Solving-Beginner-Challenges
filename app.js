edc22356eae964d5908f76324f706787461cf39d
Rich <richbport@yahoo.com>
2025-05-08 17:50:55 -0400

    added setTimeout

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 function getSubscriptionStatus() {
   return new Promise((resolve, reject) => {
-    resolve("VIP");
+    setTimeout(() => {
+      resolve("VIP");
+    }, 2000);
   });
 }
+
+getSubscriptionStatus().then((response) => console.log(response));
 
 async function main() {
   console.log(await getSubscriptionStatus());
