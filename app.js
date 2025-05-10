74ee957e55ee11e09c70e14ab58ed7074c0c1285
Rich <richbport@yahoo.com>
2025-05-10 15:37:41 -0400

    Q6 first shot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function appendFrontend(num) {
-  return 4 + num;
+function sumGreaterThan100(num1, num2) {
+  return num1 + num2;
 }
 
-console.log(appendFrontend(1));
+console.log(sumGreaterThan100(50, 51));
